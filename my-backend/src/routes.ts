ab68d8b2c31fc7ecf317f358f2199fbde9585d4a
Bárbara_Marcello_02 <134644032+barbaram02@users.noreply.github.com>
2025-09-16 20:42:16 -0300

    Juntando a criação de usuário com o gerador de token.

--- my-backend/src/routes.ts
+++ my-backend/src/routes.ts
@@ -1,14 +1,12 @@
 import { Router } from "express";
 
 
-import { AuthUserController } from "./controllers/users/AuthUserController";
 import { CreateUserController } from "./controllers/users/CreateUserController";
 
 const router = Router();
 
 //rotas de usuario
 router.post('/user', new CreateUserController().handle)
-router.post('/sessions', new AuthUserController().handle)
 
 
 
