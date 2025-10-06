37d398e9043d8eb24edeac6cb3f5086d13186e25
Bárbara_Marcello_02 <134644032+barbaram02@users.noreply.github.com>
2025-10-06 18:48:33 -0300

    [Itens personalizados] Adicionei itens personalizados da nossa logo

---
+++ my-frontend/src/app/components/header/index.tsx
@@ -0,0 +1,5 @@
+export function Header(){
+    return(
+        <main>Header</main>
+    )
+}
--- my-frontend/src/app/page.tsx
+++ my-frontend/src/app/page.tsx
@@ -1,6 +1,24 @@
+import { Header } from './components/header';
+import styles  from './page.module.scss'
+import Image from 'next/image';
+import logoImg from '/public/mg-legacy-tech-lg.svg';
 
 export default function Home() {
   return (
-    <main>Página Inicial</main>
+    <>
+      <div className={styles.container}>
+        <Image
+        src={logoImg}
+        alt='Logo da Empresa'
+        width={500}
+        />
+
+        <Header/>
+
+        <div className={styles.titulo}>
+          <h1>Olá! Nós somos MG Legacy</h1>
+        </div>
+      </div>
+    </>
   );
 }
