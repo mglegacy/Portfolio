ae76217a0877a9a1fe12935c1b476aaaf5dcee64
mglegacy <mglegacyofc@gmail.com>
2025-10-14 19:29:29 -0300

    [Erro no date .CSS] - Corrigindo

--- my-frontend/src/app/page.tsx
+++ my-frontend/src/app/page.tsx
@@ -1,6 +1,8 @@
 import styles  from './page.module.scss'
 import Image from 'next/image';
 import { api } from '@/services/api'
+import {CalendarArrowUp} from 'lucide-react'
+
 interface Project {
   id: string;
   titulo: string;
@@ -39,17 +41,32 @@ export default  async function Home() {
     </header>
 
     <section className={styles.containerBody}>
-      <h1>Projects</h1>
-
       <section className={styles.listProjects}>
+
+        <h1>Projects</h1>
+
         <div className={styles.projectsGrid}>
           {projects.map((project: Project) => (
             <article key={project.id} className={styles.card}>
               <header>
-                <h3>{project.titulo}</h3>
-                <h4 className={styles.type}>{project.tipo}</h4>
-              </header>
 
+                <h3>{project.titulo}</h3>    
+
+                <h4 className={styles.type}>{project.tipo}</h4>  {/* // caixa de calendario */}
+              <div className={styles.date}>
+
+               <CalendarArrowUp size={16} color="#000000ff"/>    
+
+                {new Date(project.created_at).toLocaleDateString('pt-BR', {
+                  month: 'long',
+                  year: 'numeric',
+                })}
+                
+              </div>     
+
+
+              </header>
+              
               <figure className={styles.imageContainer}>
                 <Image
                   src={`http://localhost:8000/files/${project.banner}`}
@@ -60,8 +77,9 @@ export default  async function Home() {
 
               <p className={styles.descricao}>{project.description}</p>
 
-              <div className={styles.tecnologiesContainer}>
+              <div className={styles.tecnologiesContainer}>     {/* tecnologias usadas no projeto */}
                 {project.tecnologies.map((tech, index) => (
+
                   <span key={index} className={styles.techTag}>
                     {tech}
                   </span>
