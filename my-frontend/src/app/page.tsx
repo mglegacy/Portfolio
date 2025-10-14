7e1b6b3e8778c779c4130e325b898c41157e2d46
Bárbara_Marcello_02 <134644032+barbaram02@users.noreply.github.com>
2025-10-14 19:42:47 -0300

    [Correção 1/2] - Consegui colocar todos no final.

--- my-frontend/src/app/page.tsx
+++ my-frontend/src/app/page.tsx
@@ -48,24 +48,18 @@ export default  async function Home() {
         <div className={styles.projectsGrid}>
           {projects.map((project: Project) => (
             <article key={project.id} className={styles.card}>
-              <header>
-
-                <h3>{project.titulo}</h3>    
-
-                <h4 className={styles.type}>{project.tipo}</h4>  {/* // caixa de calendario */}
-              <div className={styles.date}>
-
-               <CalendarArrowUp size={16} color="#000000ff"/>    
-
-                {new Date(project.created_at).toLocaleDateString('pt-BR', {
-                  month: 'long',
-                  year: 'numeric',
-                })}
-                
-              </div>     
-
-
-              </header>
+              <h3>{project.titulo}</h3>
+              
+               <div className={styles.infoLine}>
+    <h4 className={styles.type}>{project.tipo}</h4>
+    <div className={styles.date}>
+      <CalendarArrowUp size={16} color="#000000ff" />
+      {new Date(project.created_at).toLocaleDateString('pt-BR', {
+        month: 'long',
+        year: 'numeric',
+      })}
+    </div>
+  </div>
               
               <figure className={styles.imageContainer}>
                 <Image
